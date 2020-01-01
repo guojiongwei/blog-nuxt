89e9d99cd256f42cfda5d37920da0d9f0d875b95
guojiongwei <18530633992@163.com>
2020-01-01 23:12:15 +0800

    处理客户端跨域问题

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,14 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
-axios.defaults.baseURL = process.env.NODE_ENV == 'production' ? 'http://localhost:444/' : 'http://localhost:444/'
-function apiGet (url, params) {
-    return axios.get(url, params).then(res => {
-        return res
-    }).catch(err => {
-        let data = {data: []}
-        return data
-    })
-}
+
  
 // let main = {
 //     install(Vue) {
@@ -20,6 +12,15 @@ function apiGet (url, params) {
  
 // 这里是 为了在 asyncData 方法中使用
 export default ({ app  }) => {
+    axios.defaults.baseURL = process.server ? 'http://localhost:444/' : '/'
+    function apiGet (url, params) {
+        return axios.get(url, params).then(res => {
+            return res
+        }).catch(err => {
+            let data = {data: []}
+            return data
+        })
+    }
     // Set the function directly on the context.app object
     // Vue.prototype.$apiGet = apiGet; // 变量的内容 后期可以在vue中 this->$api.xxx 使用
     app.$apiGet = apiGet // 名称
